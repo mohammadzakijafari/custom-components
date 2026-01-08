b9c7203e493c70a7a1b6de0c46fbdeededb52d8f
mohammadzakijafari <zaki.jafari5@gmail.com>
2026-01-08 16:54:50 +0100

    custom Button, Input and Container in custom components

---
+++ src/components/Container.tsx
@@ -0,0 +1,15 @@
+import React, { Children, type ComponentPropsWithoutRef, type ElementType, type ReactNode } from 'react'
+
+type ContainerProps<T extends ElementType> = {
+    as?: T;
+    children: ReactNode;
+} & ComponentPropsWithoutRef<T>;
+
+const Container = <C extends ElementType>({ as, children, ...props }: ContainerProps<C>) => {
+    const Component = as || "div";
+    return (
+        <Component {...props}> {children} </Component>
+    );
+}
+
+export default Container
